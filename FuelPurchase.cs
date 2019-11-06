e28df049d64f1dd5647329f4eea190706a8c4d66
Edward <edwardhoangvo@Gmail.com>
2019-11-06 23:00:30 +0800

    Initialize C# base code

---
+++ FuelPurchase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_ASS2
+{
+    class FuelPurchase
+    {
+        private double fuelEconomy;
+        private double litres = 0;
+        private double cost = 0;
+
+        public double getFuelEconomy()
+        {
+            return fuelEconomy;
+            //return this.cost / this.litres;
+        }
+
+        public double getFuel()
+        {
+            return this.litres;
+        }
+
+        public void setFuelEconomy(double fuelEconomy)
+        {
+            this.fuelEconomy = fuelEconomy;
+        }
+        public void purchaseFuel(double amount, double price)
+        {
+            this.litres += amount;
+            this.cost += price;
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_ASS2
+{
+    class Vehicle
+    {
+        private string manufacturer;
+        private string model;
+        private int makeYear;
+        // TODO add Registration Number
+        private int registrationNum;
+        // TODO add variable for OdometerReading (in KM),
+        private int odometerReading;
+        // TODO add variable for TankCapacity (in litres)
+        private int tankCapacity;
+
+        private FuelPurchase fuelPurchase;
+
+        /**
+         * Class constructor specifying name of make (manufacturer), model and year
+         * of make.
+         * @param manufacturer
+         * @param model
+         * @param makeYear
+         */
+        public Vehicle(String manufacturer, String model, int makeYear, int registrationNum,int odometerReading, int tankCapacity)
+        {
+            this.manufacturer = manufacturer;
+            this.model = model;
+            this.makeYear = makeYear;
+            this.registrationNum = registrationNum;
+            this.odometerReading = odometerReading;
+            this.tankCapacity = tankCapacity;
+
+            fuelPurchase = new FuelPurchase();
+        }
+
+        // TODO Add missing getter and setter methods
+
+        /**
+         * Prints details for {@link Vehicle}
+         */
+        public void printDetails()
+        {
+            Console.WriteLine("Vehicle: " + makeYear + " " + manufacturer + " " + model + " " + registrationNum + " " +
+              + odometerReading + " " + tankCapacity);
+            // TODO Display additional information about this vehicle
+        }
+
+
+        // TODO Create an addKilometers method which takes a parameter for distance travelled 
+        // and adds it to the odometer reading. 
+        public int addKilometers(int distanceTraveled)
+        {
+            odometerReading += distanceTraveled;
+            return odometerReading; 
+        }
+
+        // adds fuel to the car
+        public void addFuel(double litres, double price)
+        {
+            fuelPurchase.purchaseFuel(litres, price);
+        }
+    }
+}
