5705d01b2c673d9d3188264dcfe19de047c1fe4d
Edward <edwardhoangvo@Gmail.com>
2019-11-14 01:30:11 +0800

    Initialize code for Add Button

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,53 @@ namespace TDD_ASS2
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        //xxxxxxxx
+
+        ObservableCollection<Vehicle> vehicles = new ObservableCollection<Vehicle>();
+        public Vehicle xVehicle;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            xVehicle = new Vehicle();
         }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Details enterDetails = new Details();
+
+            xVehicle.Manufacturer = "";
+            xVehicle.Model = "";
+            xVehicle.MakeYear = 0;
+            xVehicle.RegistrationNum = 0;
+            xVehicle.OdometerReading = 0;
+            xVehicle.TankCapacity = 0;
+
+            enterDetails.VehicleInfo = xVehicle;
+
+            enterDetails.Owner = this;
+
+            enterDetails.WindowStartupLocation = WindowStartupLocation.Manual;
+
+            enterDetails.ShowDialog();
+
+            this.xVehicle = enterDetails.VehicleInfo;
+
+            enterDetails.Close();
+
+            vehicles.Add(new Vehicle() {
+                Manufacturer = xVehicle.Manufacturer,
+                Model = xVehicle.Model,
+                MakeYear =  xVehicle.MakeYear,
+                RegistrationNum = xVehicle.RegistrationNum,
+                OdometerReading = xVehicle.OdometerReading,
+                TankCapacity = xVehicle.TankCapacity });
+
+            VehicleList.ItemsSource = vehicles;
+         
+        }
+
     }
 }
