b4038505cd30da84fd76b9e8aafdbd1277c19af9
Edward <edwardhoangvo@Gmail.com>
2019-11-14 01:31:53 +0800

    Initialize code to get data from the form

--- Details.xaml.cs
+++ Details.xaml.cs
@@ -23,5 +23,68 @@ namespace TDD_ASS2
         {
             InitializeComponent();
         }
+
+
+        public Vehicle VehicleInfo
+        {
+            get;
+            set;
+        }
+
+        public string Manufacturer
+        {
+            get { return VehicleInfo.Manufacturer; }
+            set { VehicleInfo.Manufacturer = value; }
+        }
+
+        public int MakeYear
+        {
+            get { return VehicleInfo.MakeYear; }
+            set { VehicleInfo.MakeYear = value; }
+        }
+
+        public int RegistrationNum
+        {
+            get { return VehicleInfo.RegistrationNum; }
+            set { VehicleInfo.RegistrationNum = value; }
+        }
+
+        public int OdometerReading
+        {
+            get { return VehicleInfo.OdometerReading; }
+            set { VehicleInfo.OdometerReading = value; }
+        }
+
+        public int TankCapacity
+        {
+            get { return VehicleInfo.TankCapacity; }
+            set { VehicleInfo.TankCapacity = value; }
+        }
+
+        public string Model
+        {
+            get { return VehicleInfo.Model; }
+            set { VehicleInfo.Model = value; }
+        }
+
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+         
+
+            int year = int.Parse(YearTB.Text);
+            int num = int.Parse(NumTB.Text);
+            int odo = int.Parse(OdoTB.Text);
+            int tank = int.Parse(TankTB.Text);
+
+            Manufacturer = ManuTB.Text;
+            Model = ModelTB.Text;
+            MakeYear = year;
+            RegistrationNum = num;
+            OdometerReading = odo;
+            TankCapacity = tank;
+
+            this.Visibility = Visibility.Hidden;
+        }
     }
 }
